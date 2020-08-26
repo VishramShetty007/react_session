73ed4ffc9e6262f72e9052e75ac8f55807fad738
Guruprasad BR <brguru90@gmail.com>
2020-08-26 15:56:16 +0530

    lefe cycle mthods,state & props

---
+++ src/components/Page2/child.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+export default class parent extends Component {
+
+    state = {
+        backgroundColor: "white",
+    }
+
+
+    //life cycle methods
+
+    componentWillMount(){
+        alert("before rendering elemnts")
+
+    }
+
+    componentDidMount(){
+        alert("after rendering elemnts")
+        this.setState({backgroundColor:this.props.bg_color})
+    }
+
+    componentWillUpdate(){
+        // alert("before updating component")
+        console.log("before component update/render on state value change")
+    }
+
+
+    componentDidUpdate(){
+        console.log("component update/render on state value change")
+    }
+
+    componentWillUnmount(){
+        alert("before leaving component")
+    }
+
+    render() {
+        return (
+            <div className="ChildComp" style={{backgroundColor:this.state.backgroundColor}}>
+                <input type="text" onChange={(e) => this.setState({ backgroundColor: e.target.value })} /><br />
+                {this.state.backgroundColor}
+                <NavLink to='page1'>Page1</NavLink>
+
+            </div>
+        )
+    }
+}
--- src/components/Page2/page2.jsx
+++ src/components/Page2/page2.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import './page2.scss'
+import ChildComponent from "./child.jsx"
 
 export default class page2 extends Component {
+
+  
+   
+
+
    render() {
       return (
-         <div className="page2_div">
+         <div className="page2_div"  >
             <h1>This is page2 html</h1>
+            <ChildComponent bg_color="skyblue" />
          </div>
       )
    }
